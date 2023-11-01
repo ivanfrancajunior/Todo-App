6faceebd9b4c95ca5a32470e887462c9e9cc89c1
Ivan <contato.ivanfrancajr@gmail.com>
2023-11-01 10:20:20 -0300

    fix: Hearder - corige espaçamento | retirado hr

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import profileImage from "../assets/images/photo.png";
 const Header = () => {
   return (
     <>
-      <section className="flex items-center justify-between container mx-auto px-7 md:px-20 ">
+      <section className="flex items-center justify-between container mx-auto px-7 pt-10 md:px-20 max-w-[900px]">
         <div className="flex flex-col md:flex items-start justify-between w-full h-14 gap-2  mb-10">
           <h2 className="text-2xl font-semibold text-[#F4F6FA] my-2">To do List</h2>
           <p>Adicione uma tarefa</p>
@@ -15,7 +15,7 @@ const Header = () => {
           className="h-[52px] w-[52px] rounded-full"
         />
       </section>
-      <hr className=" mx-auto w-[calc(100%-60px)] md:w-[calc(100%-160px)] border-zinc-500 border-1 my -5" />
+      
     </>
   );
 };
